43e2bd5484f8210ca6b0eb64ff5b41f323c27ebf
sumanth7767 <95140855+sumanth7767@users.noreply.github.com>
2023-01-08 12:29:27 +0530

    Delete index.js

--- index.js
+++
@@ -1,6 +0,0 @@
-/* eslint-disable semi */
-const app = require('./app');
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-  console.log(`Started express sever on port ${PORT}`);
-});
