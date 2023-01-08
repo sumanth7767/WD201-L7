65fba3776ddf0f9ff101e5dbd1b393afbbc63509
sumanth7767 <95140855+sumanth7767@users.noreply.github.com>
2023-01-08 12:31:49 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,6 @@
+/* eslint-disable semi */
+const app = require('./app');
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Started express sever on port ${PORT}`);
+});
